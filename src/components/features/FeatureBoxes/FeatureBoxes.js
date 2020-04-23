ef0b22f2d32974f2803b5eaeaa7d6a261a454c91
JakubOlkowicz <olkuba@poczta.fm>
2020-04-23 21:42:35 +0200

    add RWD to component FeatureBox

--- src/components/features/FeatureBoxes/FeatureBoxes.js
+++ src/components/features/FeatureBoxes/FeatureBoxes.js
@@ -8,32 +8,32 @@ import {
   faBullhorn,
 } from '@fortawesome/free-solid-svg-icons';
 
-import styles from './FeatureBoxes.module.scss';
 import FeatureBox from '../../common/FeatureBox/FeatureBox';
+import styles from './FeatureBoxes.module.scss';
 
 const FeatureBoxes = () => (
   <div className={styles.root}>
     <div className='container'>
       <div className='row'>
-        <div className='col'>
+        <div className='col-md-3 col-sm-6'>
           <FeatureBox icon={faTruck} active>
             <a href='#'>Free shipping</a>
             <p>All orders</p>
           </FeatureBox>
         </div>
-        <div className='col'>
+        <div className='col-md-3 col-sm-6'>
           <FeatureBox icon={faHeadphones}>
             <a href='#'>24/7 customer</a>
             <p>support</p>
           </FeatureBox>
         </div>
-        <div className='col'>
+        <div className='col-md-3 col-sm-6'>
           <FeatureBox icon={faReplyAll}>
             <a href='#'>Money back</a>
             <p>guarantee</p>
           </FeatureBox>
         </div>
-        <div className='col'>
+        <div className='col-md-3 col-sm-6'>
           <FeatureBox icon={faBullhorn}>
             <a href='#'>Member discount</a>
             <p>First order</p>
