953b4cae168a4f47231712e23f2c4f287062278c
Nadja Brandys <nadiusza@gmail.com>
2020-04-26 15:28:32 +0200

    Add functionality to Brands - not working

--- src/components/features/Brands/Brands.js
+++ src/components/features/Brands/Brands.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import BrandsBox from '../../common/BrandBox/BrandBox';
-
+import SwipeWrapper from '../../common/SwipeWrapper/SwipeWrapper';
 import styles from './Brands.module.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
@@ -10,38 +10,112 @@ import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons
 class Brands extends React.Component {
   state = {
     activeBrands: 6,
+    startIndex: 1,
+    finishIndex: 2,
+    toRight: false,
+    toLeft: true,
   };
+
+  nextButton() {
+    const { startIndex, finishIndex } = this.state;
+    const brandCount = {
+      desktops: 4,
+      tablets: 2,
+      phones: 0,
+    };
+    const brands = this.state.brands;
+
+    if (finishIndex < brands.length - brandCount[this.props.windowMode] - 1) {
+      this.setState({
+        startIndex: startIndex + 1,
+        finishIndex: finishIndex + 1,
+      });
+    } else {
+      this.setState({
+        toRight: true,
+      });
+    }
+    this.setState({
+      toLeft: false,
+    });
+  }
+  prevButton() {
+    const { startIndex, finishIndex } = this.state;
+
+    if (startIndex > 1 && finishIndex > 0) {
+      this.setState({
+        startIndex: startIndex - 1,
+        finishIndex: finishIndex - 1,
+      });
+    } else {
+      this.setState({
+        toLeft: true,
+      });
+    }
+    this.setState({
+      toRight: false,
+    });
+  }
+
   render() {
-    const { brands } = this.props;
-    const { activeBrands } = this.state;
+    const { brands, windowMode } = this.props;
+    const { startIndex, finishIndex } = this.state;
+    const brandCount = {
+      desktops: 4,
+      tablets: 2,
+      phones: 1,
+    };
+
     return (
       <div className={'container'}>
         <div className={styles.componentContainer}>
-          <div className={styles.swipe}>
-            <FontAwesomeIcon icon={faChevronLeft} className={styles.icon} />
-          </div>
-          {brands.slice(0, activeBrands).map(brands => (
-            <div key={brands.id} className={styles.brandContainer}>
-              <BrandsBox {...brands} />
+          <SwipeWrapper
+            leftAction={() => this.nextButton()}
+            rightAction={() => this.prevButton()}
+            trackMouse
+            preventDefaultTouchmoveEvent
+          >
+            <div className={styles.swipe}>
+              <FontAwesomeIcon icon={faChevronLeft} className={styles.icon} />
+              <a
+                href='#'
+                onClick={e => {
+                  e.preventDefault();
+                  this.prevButton();
+                }}
+              ></a>
             </div>
-          ))}
+            {brands
+              .slice((startIndex, finishIndex + brandCount[windowMode]))
+              .map(brands => (
+                <div key={brands.id} className={styles.brandContainer}>
+                  <BrandsBox {...brands} />
+                </div>
+              ))}
 
-          <div className={styles.swipe}>
-            <FontAwesomeIcon icon={faChevronRight} className={styles.icon} />
-          </div>
+            <div className={styles.swipe}>
+              <FontAwesomeIcon icon={faChevronRight} className={styles.icon} />
+              <a
+                href='#'
+                onClick={e => {
+                  e.preventDefault();
+                  this.nextButton();
+                }}
+              ></a>
+            </div>
+          </SwipeWrapper>
         </div>
       </div>
     );
   }
 }
+
 Brands.propTypes = {
-  brands: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number,
-      name: PropTypes.string,
-      image: PropTypes.string,
-    })
-  ),
+  brands: PropTypes.array,
+  id: PropTypes.number,
+  name: PropTypes.string,
+  image: PropTypes.string,
+  windowMode: PropTypes.string,
 };
 
 export default Brands;
--- src/components/features/Brands/BrandsContainer.js
+++ src/components/features/Brands/BrandsContainer.js
@@ -2,9 +2,11 @@ import { connect } from 'react-redux';
 import Brands from './Brands';
 
 import { getAll } from '../../../redux/brandsRedux';
+import { getMode } from '../../../redux/windowRedux';
 
 const mapStateToProps = state => ({
   brands: getAll(state),
+  windowMode: getMode(state),
 });
 
 export default connect(mapStateToProps)(Brands);
